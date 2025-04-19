060483b40c2ccb9f8edb84e2260b87afef579d3d
Al-ameen Taofeek <112866778+alameen-coder@users.noreply.github.com>
2025-04-19 20:52:07 +0100

    f

--- x_news_bot.py
+++ x_news_bot.py
@@ -4,8 +4,26 @@ import time
 import logging
 
 from flask import Flask
-from flask import Flask
+from threading import Thread
 
+# === KEEP_ALIVE SERVER ===
+app = Flask('')
+
+@app.route('/')
+def home():
+   return "Im alive!"
+
+def run():
+    app.run(host='0.0.0.0', port=8080)  
+
+def keep_alive():
+        t = Thread(target=run)
+        t.start()
+       
+
+
+from dotenv import load_dotenv
+load_dotenv()
 
 
 TELEGRAM_BOT_TOKEN = "7709846260:AAGtFq9brzDFA3obIDx1WMVEvrtOuDx3RRU"
